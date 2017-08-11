ad3bea14534ee0e495cf26b4b2748d15341b65a1
Anna <anna@MacBook-Pro-Anna.local>
2017-08-11 14:52:01 +0200

    comments added/modified

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
-var name = prompt("Enter your name"); // plepleple
+var name = prompt("Enter your name"); // pleplepleple
 alert("Hello, " + name); // this is comment
 console.log("Hello, " + name); /* this is another comment */
