e86b9afbeda60f86fae2ec06e8a543d787db5378
Anna <anna@MacBook-Pro-Anna.local>
2017-08-11 18:03:50 +0200

    first comment has changed

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
-var name = prompt("Enter your name"); // plepleplepleple
+var name = prompt("Enter your name"); // first comment
 alert("Hello, " + name); // this is comment
 console.log("Hello, " + name); /* this is another comment */
